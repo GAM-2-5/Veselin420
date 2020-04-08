6448140fc55d6101c59a41a947b2afda6eae87fa
Veselin420 <62694205+Veselin420@users.noreply.github.com>
2020-04-08 18:57:23 +0200

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -6,14 +6,22 @@ brojac=0
 for x in f:
       karte.append(x)
 
+kartad=karte[random.randint(0,51)]
+rukad=kartad
 komanda="hit"
 while (komanda=="hit"):
       karta=karte[random.randint(0,51)]
+      while karta==kartad or karta[0] in ruka:
+            karta=karte[random.randint(0,51)]
       ruka+=karta
-      print(ruka)
-      if ruka[0].isdigit():
-            brojac+=int(ruka[0])
-            print(brojac)
-      komanda=input("hit or fold?\n")
+      print("Ruka dealera:\n",rukad)
+      print("Tvoja ruka:\n",ruka)
+      if karta[0].isdigit():
+            brojac+=int(karta[0])
+      elif (karta[0]=="t"or"k"or"j"or"k"):
+            brojac+=10
+      elif karta[0]=="a":
+            brojac+=11
+      print("Bodovi:",brojac)
+      komanda=input("hit or stand?\n")
       
-  
